3783f74309c2a86eb657e76ec42be505afbb1585
Stan Powell <git@stanpowell.com>
2024-04-30 20:59:23 -0500

    v20240430

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), preact()]
 });
